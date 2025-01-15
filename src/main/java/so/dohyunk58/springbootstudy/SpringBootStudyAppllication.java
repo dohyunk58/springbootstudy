d61343c498fd10e1b2ca3d148bccdac3a0d03229
dohyunk58 <hedge3x@gmail.com>
2025-01-15 22:33:11 +0900

    First commit

---
+++ src/main/java/so/dohyunk58/springbootstudy/SpringBootStudyAppllication.java
@@ -0,0 +1,11 @@
+package so.dohyunk58.springbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudyAppllication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyAppllication.class, args);
+    }
+}
