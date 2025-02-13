2b016c0951d0ef8c5225101a8a70b5e7b9dafc32
dohyunk58 <hedge3x@gmail.com>
2025-02-14 02:30:29 +0900

    Article 객체 저장하는 save 추가

---
+++ src/main/java/so/dohyunk58/springbootstudy/service/BlogService.java
@@ -0,0 +1,18 @@
+package so.dohyunk58.springbootstudy.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import so.dohyunk58.springbootstudy.domain.Article;
+import so.dohyunk58.springbootstudy.dto.AddArticleRequest;
+import so.dohyunk58.springbootstudy.repository.BlogRepository;
+
+@RequiredArgsConstructor // final or @NotNull인 필드의 생성자 추가
+@Service // 빈 등록, 컴포넌트 스캔을 위한 것
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 Create 메서드 = save()
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
