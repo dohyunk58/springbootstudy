fdb7d28d75520c0783d97a7e7faa42cd1c704326
dohyunk58 <hedge3x@gmail.com>
2025-02-18 01:50:34 +0900

    모든 글 조회 findAll() 추가

--- src/main/java/so/dohyunk58/springbootstudy/service/BlogService.java
+++ src/main/java/so/dohyunk58/springbootstudy/service/BlogService.java
@@ -6,6 +6,8 @@ import so.dohyunk58.springbootstudy.domain.Article;
 import so.dohyunk58.springbootstudy.dto.AddArticleRequest;
 import so.dohyunk58.springbootstudy.repository.BlogRepository;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final or @NotNull인 필드의 생성자 추가
 @Service // 빈 등록, 컴포넌트 스캔을 위한 것
 public class BlogService {
@@ -14,5 +16,10 @@ public class BlogService {
     // 블로그 글 Create 메서드 = save()
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 모든 글을 조회하는 findAll()
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
