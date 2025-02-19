5d524934c3e8b7c16e7437ff11a245cf2e5d7573
dohyunk58 <hedge3x@gmail.com>
2025-02-20 00:41:40 +0900

    Article update DTO 추가

---
+++ src/main/java/so/dohyunk58/springbootstudy/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package so.dohyunk58.springbootstudy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
