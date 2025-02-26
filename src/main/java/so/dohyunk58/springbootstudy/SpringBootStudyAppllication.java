522e43f6da7048b7dfae2a9867d89efcdd1da572
dohyunk58 <hedge3x@gmail.com>
2025-02-27 00:35:59 +0900

    생성 시간, 수정 시간 자동 로딩 추가

--- src/main/java/so/dohyunk58/springbootstudy/SpringBootStudyAppllication.java
+++ src/main/java/so/dohyunk58/springbootstudy/SpringBootStudyAppllication.java
@@ -2,7 +2,9 @@ package so.dohyunk58.springbootstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootStudyAppllication {
     public static void main(String[] args) {
