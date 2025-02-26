968dd21cb7fcc281f2bb09066c50618e2f275ed3
dohyunk58 <hedge3x@gmail.com>
2025-02-26 23:17:00 +0900

    글 목록 View Controller 추가

---
+++ src/main/java/so/dohyunk58/springbootstudy/dto/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package so.dohyunk58.springbootstudy.dto;
+
+import lombok.Getter;
+import so.dohyunk58.springbootstudy.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
