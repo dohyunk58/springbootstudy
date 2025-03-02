80a48536304e98dd6ea85860ce326db037169569
dohyunk58 <hedge3x@gmail.com>
2025-03-02 17:29:38 +0900

    회원가입 DTO 추가

---
+++ src/main/java/so/dohyunk58/springbootstudy/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+package so.dohyunk58.springbootstudy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
